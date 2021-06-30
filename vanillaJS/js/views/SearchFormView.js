5f76930c52b9f122d39be72e98bd1b4fdad11bc2
giu0213@naver.com <giu0213@naver.com>
2021-06-30 23:31:12 +0900

    reset button keyup event

--- vanillaJS/js/views/SearchFormView.js
+++ vanillaJS/js/views/SearchFormView.js
@@ -1,15 +1,31 @@
 import View from './View.js';
-import { qs } from '../helpers.js';
+import { qs, on } from '../helpers.js';
 
 export default class SearchFormView extends View {
     constructor() {
         super(qs('#search-form-view'));
 
+        this.inputElement = qs('[type=text]', this.element);
         this.resetElement = qs('[type=reset]', this.element);
+
         this.showResetbtn(false);
+        this.bindEvent();
     }
 
     showResetbtn(visible = true) {
         this.resetElement.style.display = visible ? "block" : "none";
+    }
+
+    // view가 생성되었을 때 event를 binding
+    bindEvent() {
+        on(this.inputElement, "keyup", () => {
+            this.handleKeyup();
+        })
+    }
+
+    handleKeyup() {
+        // console.log(this.inputElement.value)
+        const {value} = this.inputElement;
+        this.showResetbtn(value.length > 0);
     }
 }
