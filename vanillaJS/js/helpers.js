8e91b3ca998d2052df125a0d336a953b74c7ea53
giu0213@naver.com <giu0213@naver.com>
2021-06-30 16:51:36 +0900

    요구사항1. 검색폼 만들기(완)

--- vanillaJS/js/helpers.js
+++ vanillaJS/js/helpers.js
@@ -1,19 +1,25 @@
+// DOM API MAPPING
+
+// querySelector 함수 호출 -> element 반환
 export function qs(selector, scope = document) {
   if (!selector) throw "no selector";
 
   return scope.querySelector(selector);
 }
 
+// querySelectorAll 함수 호출 -> element 반환(array로)
 export function qsAll(selector, scope = document) {
   if (!selector) throw "no selector";
 
   return Array.from(scope.querySelectorAll(selector));
 }
 
+// adEventListener 함수 호출 -> element에서 event 수신, handler 호출
 export function on(target, eventName, handler) {
   target.addEventListener(eventName, handler);
 }
 
+// on과 비슷 but handler 따로 호출 -> 특정 element 하위의 자식 element event 처리
 export function delegate(target, eventName, selector, handler) {
   const emitEvent = (event) => {
     const potentialElements = qsAll(selector, target);
@@ -28,11 +34,13 @@ export function delegate(target, eventName, selector, handler) {
   on(target, eventName, emitEvent);
 }
 
+// event 발행, dispatchEvent함수 호출
 export function emit(target, eventName, detail) {
   const event = new CustomEvent(eventName, { detail });
   target.dispatchEvent(event);
 }
 
+// Date 객체를 받아서 상대 시간 객체 반환
 export function formatRelativeDate(date = new Date()) {
   const TEN_SECOND = 10 * 1000;
   const A_MINUTE = 60 * 1000;
@@ -51,6 +59,7 @@ export function formatRelativeDate(date = new Date()) {
   });
 }
 
+// 과거 날짜 객체 반환
 export function createPastDate(date = 1, now = new Date()) {
   if (date < 1) throw "date는 1 이상입니다";
 
@@ -60,6 +69,7 @@ export function createPastDate(date = 1, now = new Date()) {
   return createPastDate(date - 1, yesterday);
 }
 
+// 하나씩 늘리면서 id를 만들기 위한 함수
 export function createNextId(list = []) {
   return Math.max(...list.map((item) => item.id)) + 1;
 }
