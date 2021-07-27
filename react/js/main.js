e095675fe519e44103c2e06110d6833b59a7fd77
giu0213@naver.com <giu0213@naver.com>
2021-07-27 17:29:52 +0900

    create tab

--- react/js/main.js
+++ react/js/main.js
@@ -1,98 +1,130 @@
-import store from './js/Store.js';
+import store from "./js/Store.js";
+
+const TabType = {
+  KERWORD: "KEYWORD",
+  HISTORY: "HISTORY",
+};
+
+const TabLabel = {
+  [TabType.KERWORD]: "추천 검색어",
+  [TabType.HISTORY]: "최근 검색어",
+};
 
 class App extends React.Component {
+  constructor() {
+    super();
 
-    constructor() {
-        super();
+    this.state = {
+      searchKeyword: "",
+      searchResult: [],
+      submitted: false,
+      selectedTab: TabType.KERWORD,
+    };
+  }
 
-        this.state = {
-            searchKeyword: '',
-            searchResult: [],
-            submitted: false,
-        };
+  search(searchKeyword) {
+    const searchResult = store.search(searchKeyword);
+    this.setState({ searchResult, submitted: true });
+  }
+
+  handleSearchKeyword(event) {
+    const searchKeyword = event.target.value;
+
+    if (searchKeyword.length <= 0) {
+      return this.handleReset();
     }
 
-    search(searchKeyword) {
-        const searchResult = store.search(searchKeyword);
-        this.setState({ searchResult, submitted: true });
-    }
+    this.setState({
+      searchKeyword,
+    });
+  }
 
-    handleSearchKeyword(event) {
-        const searchKeyword = event.target.value
+  handleSubmit(event) {
+    event.preventDefault();
+    this.search(this.state.searchKeyword);
+  }
 
-        if (searchKeyword.length <= 0) {
-            return this.handleReset();
-        }
+  handleReset() {
+    this.setState(
+      () => {
+        return { searchKeyword: "", submitted: false };
+      },
+      () => {
+        console.log("reset-event", this.state.searchKeyword);
+      },
+    );
+  }
 
-        this.setState({
-            searchKeyword
-        });
-    }
+  render() {
+    const searchForm = (
+      <form
+        onSubmit={(event) => this.handleSubmit(event)}
+        onReset={() => this.handleReset()}
+      >
+        <input
+          type="text"
+          placeholder="검색어를 입력하세요"
+          autoFocus
+          value={this.state.searchKeyword}
+          onChange={(event) => this.handleSearchKeyword(event)}
+        />
+        {this.state.searchKeyword.length > 0 && (
+          <button type="reset" className="btn-reset"></button>
+        )}
+      </form>
+    );
 
-    handleSubmit(event) {
-        event.preventDefault();
-        this.search(this.state.searchKeyword);
-    }
+    const searchResult =
+      this.state.searchResult.length > 0 ? (
+        <ul className="result">
+          {this.state.searchResult.map((item) => {
+            return (
+              <li key={item.id}>
+                <img src={item.imageUrl} alt={item.name} />
+                <p>{item.name}</p>
+              </li>
+            );
+          })}
+        </ul>
+      ) : (
+        <div className="empty-box">검색 결과가 없습니다.</div>
+      );
 
-    handleReset() {
-        this.setState(() => {
-            return { searchKeyword: '', submitted: false };
-        }, () => {
-            console.log('reset-event', this.state.searchKeyword)
-        });
-    }
+    const tabs = (
+      <>
+        <ul className="tabs">
+          {Object.values(TabType).map((tabType) => {
+            return (
+              <li
+                className={this.state.selectedTab === tabType ? "active" : ""}
+                key={tabType}
+                onClick={() => this.setState({ selectedTab: tabType })}
+              >
+                {TabLabel[tabType]}
+              </li>
+            );
+          })}
+        </ul>
+        {this.state.selectedTab === TabType.KERWORD && <>추천 검색어</>}
+        {this.state.selectedTab === TabType.HISTORY && <>최근 검색어</>}
+      </>
+    );
 
-    render() {
+    return (
+      <>
+        <header>
+          <h2 className="container">검색</h2>
+        </header>
 
-        const searchForm = (
-            <form
-                onSubmit={event => this.handleSubmit(event)}
-                onReset={() => this.handleReset()}
-            >
-                <input
-                    type="text"
-                    placeholder="검색어를 입력하세요"
-                    autoFocus
-                    value={this.state.searchKeyword}
-                    onChange={event => this.handleSearchKeyword(event)}
-                />
-                {
-                    this.state.searchKeyword.length > 0 &&
-                    (<button type="reset" className="btn-reset"></button>)
-                }
-            </form>
-        )
-
-        const searchResult = (
-            this.state.searchResult.length > 0
-            ? <ul className="result">
-                {this.state.searchResult.map(item => {
-                    return (
-                        <li key={item.id}>
-                            <img src={item.imageUrl} alt={item.name} />
-                            <p>{item.name}</p>
-                        </li>
-                    );
-                })}
-              </ul>
-            : <div className="empty-box">검색 결과가 없습니다.</div>
-        )
-
-        return (
-            <>
-                <header>
-                    <h2 className="container">검색</h2>
-                </header>
-
-                <div className="container">
-                    { searchForm }
-                    <div className="content">
-                        { this.state.submitted && searchResult }
-                    </div>
-                </div>
-            </>
-        )
-    }
+        <div className="container">
+          {searchForm}
+          <div className="content">
+            {this.state.submitted ? searchResult : tabs}
+          </div>
+        </div>
+      </>
+    );
+  }
 }
 
-ReactDOM.render(<App />, document.querySelector('#app'));
+ReactDOM.render(<App />, document.querySelector("#app"));
