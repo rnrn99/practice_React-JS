714e04fd107b67f2e34089c0ef90b2a8fd98e376
giu0213@naver.com <giu0213@naver.com>
2021-08-03 15:14:53 +0900

    make header recyclable

--- component/src/App.js
+++ component/src/App.js
@@ -1,8 +1,8 @@
 import React from "react";
-import Header from "./components/Header";
+import Header from "./components/Header.js";
 
 export default class App extends React.Component {
   render() {
-    return <Header />;
+    return <Header title="검색" />;
   }
 }
