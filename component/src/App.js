d97759783c0146db5a97cadec19af4c5846c3e65
giu0213@naver.com <giu0213@naver.com>
2021-08-03 14:34:59 +0900

    create header with functional component

--- component/src/App.js
+++ component/src/App.js
@@ -1,7 +1,8 @@
 import React from "react";
+import Header from "./components/Header";
 
 export default class App extends React.Component {
   render() {
-    return <div>App</div>;
+    return <Header />;
   }
 }
